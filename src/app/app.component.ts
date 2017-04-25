bb033293f7ca283121c0e4dc2035f05585927822
Ricardo Oyarzo M <raomaster@gmail.com>
2017-04-24 22:23:47 -0300

    usando map para reescribir datos de una consultra de forma local

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import {AngularFire, FirebaseListObservable} from 'angularfire2';
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/operator/map';
+
 
 @Component({
   selector: 'app-root',
@@ -9,15 +13,23 @@ import {AngularFire, FirebaseListObservable} from 'angularfire2';
 export class AppComponent {
   title = 'app works!';
   couisines: FirebaseListObservable<any[]>;
-  restaurant;
+  restaurants: Observable<any[]>;
   constructor(private af: AngularFire) {
   }
 
   ngOnInit() {
     // Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     // Add 'implements OnInit' to the class.
-   this.couisines = this.af.database.list('/couisines');
-   this.restaurant = this.af.database.object('/restaurant');
+   this.couisines = this.af.database.list('/cuisines');
+   this.restaurants = this.af.database.list('/restaurants')
+                      .map(restaurants => {
+                          console.log("BEFORE MAP", restaurants);
+                          restaurants.map(restaurant => {
+                            restaurant.cusineType = this.af.database.object('/cuisines/' + restaurant.cuisine);
+                          });
+                          console.log('AFTER MAP', restaurants);
+                          return restaurants;
+                      });
 
   }
 
